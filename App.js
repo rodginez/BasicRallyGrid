efb453f5407768b82f7c336138e692a174e2a146
Rodrigo Ginez <rodginez@gmail.com>
2016-04-15 14:49:16 -0300

    InteractiveGrid

--- App.js
+++ App.js
@@ -3,20 +3,35 @@ var myApp = Ext.define('CustomApp', {
     componentCls: 'app',
   //launch is called implicitly
     launch: function() {
-      var store = this._loadData();
-      this._loadGrid(store);
+      //var store = this._loadData();
+      this._loadIterations();
     },
-    _loadData: function(){
+
+    _loadIterations: function(){
+      this.iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+        listeners: {
+          single: true,
+          ready: this._loadData,
+          scope: this
+        }
+      });
+      this.add(this.iterComboBox);
+
+    },
+    _loadData: function(component){
+      var selectedRef = component.getRecord().get('_ref');
+
       var myStore = Ext.create('Rally.data.wsapi.Store', {
-          model: 'User Story',
+          model: 'Defect',
           autoLoad: true,
           listeners:{
               load: function(store, data, success) {
-                console.log('got data', myStore, data, success);
+                //console.log('got data', myStore, data, success);
                 //Grid has to be loaded from within the load function - asynchronous
+                this._loadGrid(myStore)
               }, scope: this
           },
-          fetch: ['FormattedID', 'Name', 'ScheduleState']
+          fetch: ['FormattedID', 'Name', 'ScheduleState',  'Blocked', 'Severity', 'Iteration']
         }
       );
       return myStore;
@@ -24,14 +39,13 @@ var myApp = Ext.define('CustomApp', {
     _loadGrid: function(store){
       var myGrid = Ext.create('Rally.ui.grid.Grid', {
         store: store,
-        columnCfgs: ['FormattedID', 'Name', 'ScheduleState']
+        columnCfgs: ['FormattedID', 'Name', 'ScheduleState', 'Blocked', 'Severity', 'Iteration']
       });
       //Two different ways
       //First: explicitly refer to the app
       //Second: use the scope attribute to set this to the app
       //(at the time of the function creation this refers to the app!
       //myApp.add(myGrid);
-
       this.add(myGrid);
     }
 });
